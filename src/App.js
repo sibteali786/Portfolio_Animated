a873d7dcc78cbe502a03b30b4af438f0b6bd7f91
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-08 22:50:33 +0500

    made needed changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import { useRef, useEffect } from "react";
 
 function App() {
+  // const inputEl = useRef(null);
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: "translateX(0px)" },
+      { transform: "translateX(600px)" },
+    ],
+    timing: {
+      duration: 3000,
+    },
+  });
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello World </h1>
+      <input ref={inputEl} type="text" /> */}
+      <div ref={ref}>Hello World</div>
     </div>
   );
 }
