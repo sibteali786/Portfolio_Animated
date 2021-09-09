5e0994dfb81646f735d242417d1e6b4e51ef0073
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-09 09:36:51 +0500

    demonstarted useof getAnimation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
-  const { ref, getAnimation } = useWebAnimations({
+  const { ref, playState, getAnimation } = useWebAnimations({
     keyframes: [
       { transform: "translateX(-100%)", opacity: 0 },
       { transform: "translateX(0%)", opacity: 1 },
@@ -16,7 +16,22 @@ function App() {
   });
   return (
     <div className="App">
+      <p>Animation Running {playState} </p>
       <h1 ref={ref}>Hello World</h1>
+      <button
+        onClick={() => {
+          getAnimation().play();
+        }}
+      >
+        Play
+      </button>
+      <button
+        onClick={() => {
+          getAnimation().pause();
+        }}
+      >
+        Pause
+      </button>
     </div>
   );
 }
