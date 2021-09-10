85135aa3ea687de8f86f2c4ecae2757c7ffed438
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-10 09:05:45 +0500

    added svg logo to navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ClockLoader from "react-spinners/ClockLoader";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { SibtealiBaqar, Contact, Projects, About } from "./components";
+import { ReactComponent as ReactLogo } from "./logo.svg";
+
 function App() {
   const [loading, setloading] = useState(false);
   useEffect(() => {
@@ -15,18 +17,27 @@ function App() {
     <div>
       {loading ? (
         <div className={styles.container}>
-          <ClockLoader color={"#FFFAFB"} loading={loading} size={150} />
+          <ClockLoader color={"#ee9b00"} loading={loading} size={150} />
         </div>
       ) : (
         <div className={styles.container} style={{ color: "#FFFAFB" }}>
           <Router>
             <nav className={(styles.navBar, styles.hel)}>
-              <h1 style={{ letterSpacing: "0.5rem " }}>
-                <i class="fa fa-code"></i>
-                Sibteali Baqar
-              </h1>
+              <Link to="/">
+                <h1
+                  style={{
+                    letterSpacing: "0.3rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "3rem",
+                  }}
+                >
+                  <ReactLogo style={{ marginRight: "0.8rem" }} />
+                  ibteali Baqar
+                </h1>
+              </Link>
               <ul>
-                <Link to="/">SibtealiBaqar</Link>
                 <Link to="projects">Projects</Link>
                 <Link to="about">About</Link>
                 <Link to="contact">Contact</Link>
