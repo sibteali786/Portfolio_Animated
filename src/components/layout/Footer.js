8d15fad0bf8e49b4f0dfd3f723cdd336eb2136ab
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-01-17 06:36:27 +0500

    done some more changes and added prettier configuration

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,37 +1,36 @@
-import React from 'react'
+import React from "react";
 import styles from "../../Sass/components/footer.module.scss";
 import { ReactComponent as ReactLogo } from "../../Resources/logo_2.svg";
-function Footer(){
-
-
-
-    return (
-        <div className = {`${styles.footer}`}>
-            <ReactLogo className={`logo`} />
-            <div>
-            <h6>
-                Services
-            </h6>
-            <p>
-                Web Design 
-            </p>
-            <p>
-                MERN Stack Web Development 
-            </p>
-            <p>
-                Serverless  
-            </p>
-            </div>
-            <div>
-            <h6>
-                Contact
-            </h6>
-                <a href="">Sibteali786@gmail.com</a>
-                <a href="">ssibteali.ce41ceme@ce.ceme.edu.pk</a>
-            </div>
-            
-        </div>
-    )
+function Footer() {
+  return (
+    <div className={`${styles.footer}`}>
+      <ReactLogo className={`logo`} />
+      <div>
+        <h6>Company</h6>
+        <p>Projects</p>
+        <p>About</p>
+        <p>Blog</p>
+      </div>
+      <div>
+        <h6>Services</h6>
+        <p>Web Design</p>
+        <p>MERN Stack Web Development</p>
+        <p>Serverless</p>
+        <p>ReactJS Development</p>
+        <p>Responsive Web Design</p>
+        <p>NodeJS Development</p>
+      </div>
+      <div>
+        <h6>Contact</h6>
+        <p>
+          <a href="">Sibteali786@gmail.com</a>
+        </p>
+        <p>
+          <a href="">ssibteali.ce41ceme@ce.ceme.edu.pk</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
