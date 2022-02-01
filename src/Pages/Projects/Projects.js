2ef75c4a93533810420e290738b8019e6699a0f0
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-02-01 23:47:49 +0500

    using api endpoints

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -3,13 +3,14 @@ import styles from "../../Sass/components/projects.module.scss";
 export default Projects;
 
 function Projects() {
-  const [user, setUser] = useState([]);
-  const URL = `https://api.github.com/users/sibteali786/repos`;
+  const [repos, setrepos] = useState([]);
+  const URL = `https://api.github.com/users/sibteali786/starred`;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
     const res = await fetch(URL);
     const data = await res.json();
-    setUser(data);
+    console.log(data);
+    setrepos(data);
   }, []);
   return (
     <div className={`${styles.illuminationTop}  ${styles.illuminationTopLeft}`}>
@@ -22,7 +23,24 @@ function Projects() {
           </h1>
           <div className={`${styles.projectItem}`}>
             <div className={`${styles.projectItemImage}`}></div>
-            <div className={`${styles.projectItemText}`}></div>
+            <div className={`${styles.projectItemText}`}>
+              {repos.map((repo) => (
+                <div key={repo.id}>
+                  <p>{repo.description}</p>
+                  <h3>{repo.full_name}</h3>
+                  <a href={repo.url}>view project</a>
+                  <div>
+                    <a
+                      href={repo.languages_url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Languages
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </main>
