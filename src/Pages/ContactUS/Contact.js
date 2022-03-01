ee1d59177b527122178caefbd61d87e61c73d721
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-03-01 20:51:02 +0500

    done some changes in css

--- src/Pages/ContactUS/Contact.js
+++ src/Pages/ContactUS/Contact.js
@@ -5,13 +5,31 @@ export default Contact;
 
 function Contact() {
   return (
-    <main>
+    <main className={`${styles.main}`}>
       <h1
         className="title text-gradient title-big"
         style={{ fontWeight: "700" }}
       >
         Contact Us
       </h1>
+      <div className={`${styles.infoSection}`}>
+        <div className={`${styles.leftColumn}`}>
+          <p className={`${styles.BlackText}`}>Email us:</p>
+          <a
+            href="mailto:sibteali786@gmail.com"
+            target="_blank"
+            className={` underlined-text mail-to-us-button ${styles.BlackText}`}
+            rel="noreferrer"
+            style={{ color: "transparent" }}
+          >
+            sibteali786@gmail.com
+          </a>
+        </div>
+        <div className={`${styles.rightColumn}`}>
+          Open for suggestion, ideas, discussion and needs. Constructive
+          criticism is appreciated.
+        </div>
+      </div>
     </main>
   );
 }
